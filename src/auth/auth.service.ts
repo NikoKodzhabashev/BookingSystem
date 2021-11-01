64e4869a06aa0d01fc414b8b94ba259d013bb2e0
Niko <nikokodzhabashev@gmail.com>
2021-11-01 23:53:37 +0200

    feat(auth.service): auth service
    added fn to create jwt token, hash/compare password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
+export interface JWTPayload {
+  data: {
+    id: number;
+    email: string;
+  };
+  iat: number;
+  exp: number;
+}
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  generateJwt(user: { id: number; email: string }): Promise<string> {
+    return this.jwtService.signAsync({ user });
+  }
+
+  hashPassword(password: string): Promise<string> {
+    const saltRounds = 12;
+    return bcrypt.hash(password, saltRounds);
+  }
+
+  comparePasswords(
+    password: string,
+    storedPasswordHash: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(password, storedPasswordHash);
+  }
+}
