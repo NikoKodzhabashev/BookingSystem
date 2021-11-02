cb4e9187c4734ca4d62c6478fe8234c38b52ad19
Niko <nikokodzhabashev@gmail.com>
2021-11-02 10:35:09 +0200

    feat(rooms.service): added rooms service
    added function to get all rooms and related bookings

--- src/rooms/rooms.service.ts
+++ src/rooms/rooms.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class RoomsService {}
+export class RoomsService {
+  constructor(private prismaService: PrismaService) {}
+
+  async findAll() {
+    const rooms = await this.prismaService.room.findMany({
+      include: {
+        bookings: {
+          select: {
+            startTime: true,
+            endTime: true,
+          },
+        },
+      },
+    });
+
+    return rooms;
+  }
+}
